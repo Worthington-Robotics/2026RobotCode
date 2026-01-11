c544e9a762c245e12db2da88cc353806ffdaedb4
ih0027 <119897082+ih0027@users.noreply.github.com>
2026-01-11 18:03:14 -0500

    Reconciling differing folder names on github

--- src/main/java/frc/WorBots/Constants.java
+++
@@ -1,6 +0,0 @@
-package frc.WorBots;
-
-public class Constants {
-    public static final double ROBOT_PERIOD = 50.0;
-    public static final boolean IS_COMP = false;
-}
--- src/main/java/frc/WorBots/FieldConstants.java
+++
@@ -1,5 +0,0 @@
-package frc.WorBots;
-
-public class FieldConstants {
-    
-}
