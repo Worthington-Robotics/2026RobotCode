4f9b2d72b2b7ee9a65668569a223f5591285d96e
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-29 17:46:40 -0500

    ShooterIO Completed

--- src/main/java/frc/WorBots/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/WorBots/subsystems/shooter/ShooterIO.java
@@ -1,5 +1,20 @@
 package frc.WorBots.subsystems.shooter;
 
+import frc.WorBots.util.HardwareUtils.TalonInputsPositional;
+
 public interface ShooterIO {
-    
+
+    public static class ShooterIOInputs{
+        //nothing in here just yet
+        boolean isConnected = false;
+
+        public final TalonInputsPositional fly = 
+            new TalonInputsPositional("Shooter", "Flywheel");
+        public double velocityRPMfly = 0;
+    }
+    //implement things like make shooter spin at velocity
+    public void updateInputs(ShooterIOInputs input);
+
+    public void setVolts(double volts);
+
 }
