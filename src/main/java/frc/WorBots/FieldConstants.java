e641585c138078f8e0415e7055ff3aeedb5e6680
Non specific programmer 4 <worbotsprogrammer@gmail.com>
2026-01-29 15:54:56 -0500

    Flipped field bounds around so that bounding works properly

--- src/main/java/frc/WorBots/FieldConstants.java
+++ src/main/java/frc/WorBots/FieldConstants.java
@@ -3,6 +3,6 @@ package frc.WorBots;
 import edu.wpi.first.math.util.Units;
 
 public class FieldConstants {
-    public static final double fieldLength = Units.inchesToMeters(317.69);
-    public static final double fieldWidth = Units.inchesToMeters(651.22);
+    public static final double fieldWidth = Units.inchesToMeters(317.69);
+    public static final double fieldLength = Units.inchesToMeters(651.22);
 }
