cdd0c8e118535e35f7e1b84bbfb38201d439727e
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-29 17:47:12 -0500

    Shooter.java Updated

--- src/main/java/frc/WorBots/subsystems/shooter/Shooter.java
+++ src/main/java/frc/WorBots/subsystems/shooter/Shooter.java
@@ -1,8 +1,15 @@
 package frc.WorBots.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.WorBots.subsystems.shooter.ShooterIO.ShooterIOInputs;
+
 
 public class Shooter extends SubsystemBase {
-    
+    private ShooterIO io;
+    private ShooterIOInputs inputs = new ShooterIOInputs();
+    private boolean hasFuel = false;
+    private double feederWheelVolts = 0.0;
+
+
     
 }
