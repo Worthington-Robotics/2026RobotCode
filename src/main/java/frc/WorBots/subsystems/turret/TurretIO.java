5713e43eca6e8370874545a86429e15ce7b0e7f8
ih0027 <119897082+ih0027@users.noreply.github.com>
2026-01-30 16:05:32 -0500

    Added TODO's and remove Playing With Fusion

--- src/main/java/frc/WorBots/subsystems/turret/TurretIO.java
+++ src/main/java/frc/WorBots/subsystems/turret/TurretIO.java
@@ -5,7 +5,7 @@ public interface TurretIO {
     
 
   public default void setVoltage(double volts) {}
-
+//TODO change PivotIOInputs to TurretIOInputs
   public default void resetZero(PivotIOInputs inputs, double position) {}
 
   public default void updateInputs(PivotIOInputs inputs) {}
--- src/main/java/frc/WorBots/subsystems/turret/TurretIOTalon.java
+++ src/main/java/frc/WorBots/subsystems/turret/TurretIOTalon.java
@@ -6,17 +6,18 @@ import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import frc.WorBots.CanIDs;
 
 public class TurretIOTalon implements TurretIO{
+    //TODO change motor controller type to talonFX
     private Talon turretMotor;
-    private Talon turretAngle
+    private Talon turretAngle;
 
     public TurretIOTalon(){
         turretMotor = new Talon(CanIDs.TURRET_ID);
         turretAngle = new Talon(CanIDs.TURRET_ENCODER_ID);
 
+        //TODO use hardware utils to do this
         turretMotor.setNeutralMode(NeutralModeValue.Brake);
-        
 
-
+        //TODO implement TurretIOInputs
     }
 
 }
