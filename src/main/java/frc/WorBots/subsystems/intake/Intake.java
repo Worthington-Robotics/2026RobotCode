5064bf48d7fe45a257f6a21ccdec918d522b5a2d
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-31 11:43:26 -0500

    Intake.java Constructor & Publishers
    Written the constructor, alongside the publishers, with the instance variables.

--- src/main/java/frc/WorBots/subsystems/intake/Intake.java
+++ src/main/java/frc/WorBots/subsystems/intake/Intake.java
@@ -1,7 +1,40 @@
 package frc.WorBots.subsystems.intake;
 
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.WorBots.subsystems.intake.IntakeIO.IntakeIOInputs;
+//import frc.WorBots.util.debug.StatusPage;
 
 public class Intake extends SubsystemBase {
+    private final IntakeIO io;
+    private final IntakeIOInputs inputs = new IntakeIOInputs();
+
+    //Whether or not the intake has fuel within it
+    private boolean hasFuel = false;
+
+    //The setpoint voltage for the intake
+    private double setPointVoltage = 0.0;
+
+    private final NetworkTableInstance instance = NetworkTableInstance.getDefault();
+    private final NetworkTable intakeTable = instance.getTable("Intake");
+    private final DoublePublisher setpointPub =
+        intakeTable.getDoubleTopic("Setpoint Volts").publish();
+    private final BooleanPublisher hasGamePiecePub =
+        intakeTable.getBooleanTopic("Has Game Piece").publish();
+    private final DoublePublisher currentDrawPub =
+        intakeTable.getDoubleTopic("Current Draw").publish();
+
+    public Intake(IntakeIO io){
+        this.io = io;
+        //StatusPage.reportStatus(StatusPage.INTAKE_SUBSYSTEM, true);
+    }
+
     
+    
+    
+
+
 }
