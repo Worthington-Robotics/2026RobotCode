bad3b436b2920e1573b4a09704c58d5d9b4c6f81
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-31 16:27:16 -0500

    Finished IntakeIOTalon Constructor

--- src/main/java/frc/WorBots/subsystems/intake/IntakeIOTalon.java
+++ src/main/java/frc/WorBots/subsystems/intake/IntakeIOTalon.java
@@ -1,5 +1,64 @@
 package frc.WorBots.subsystems.intake;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.playingwithfusion.TimeOfFlight;
+import com.playingwithfusion.TimeOfFlight.RangingMode;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.units.measure.Current;
+import frc.WorBots.util.HardwareUtils.OptimalStatusSignal;
+import edu.wpi.first.units.measure.Current;
+import frc.WorBots.util.HardwareUtils.OptimalStatusSignal;
+import frc.WorBots.util.HardwareUtils.TalonSignalsPositional;
+import frc.WorBots.util.HardwareUtils;
+import frc.WorBots.Constants;
+import frc.WorBots.CanIDs;
+
 public class IntakeIOTalon implements IntakeIO {
+    private final TalonFX intakeMotor;
+    private final TalonFX extendingMotor;
+    private final TimeOfFlight timeOfFlight;
+
+    private final TalonSignalsPositional intakeMotorSignals;
+    private final TalonSignalsPositional extendingMotorSignals;
+
+    private final OptimalStatusSignal<Current> intakeCurrentDrawSignal;
+    private final OptimalStatusSignal<Current> extendingCurrentDrawSignal;
+
+    private final LinearFilter tofFilter = LinearFilter.movingAverage(1);
+
+    public IntakeIOTalon(){
+
+        intakeMotor = new TalonFX(CanIDs.Main.INTAKE_MOTOR_ID, Constants.MAIN_CAN_BUS );
+        extendingMotor = new TalonFX(CanIDs.Main.EXTENDING_MOTOR_ID, Constants.MAIN_CAN_BUS);
+
+
+        intakeMotor.setNeutralMode(NeutralModeValue.Brake);
+        HardwareUtils.setInverted(extendingMotor, false);
+        extendingMotor.setNeutralMode(NeutralModeValue.Brake);
+        HardwareUtils.setInverted(intakeMotor, false);
+
+        intakeMotorSignals = new TalonSignalsPositional(intakeMotor);
+        extendingMotorSignals = new TalonSignalsPositional(extendingMotor);
+
+        timeOfFlight = new TimeOfFlight(CanIDs.Main.TOF_ID);
+        timeOfFlight.setRangingMode(RangingMode.Short, 24);
+
+
+        intakeCurrentDrawSignal = 
+            new OptimalStatusSignal<>(intakeMotor.getStatorCurrent(), Constants.ROBOT_PERIOD);
+        extendingCurrentDrawSignal = 
+            new OptimalStatusSignal<>(extendingMotor.getStatorCurrent(), Constants.ROBOT_PERIOD);
+
+        HardwareUtils.setCurrentLimit(intakeMotor, 160);
+        HarwareUtils.setCurrentLimit(extendingMotor, 160);    
+
+        intakeMotor.optimizeBusUtilization();
+        extendingMotor.optimizeBusUtilization();
+
+    }
+
+
     
 }
