05bd39b17fb5d656b1fa30161ff4fe556dc6f1cb
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-31 16:12:22 -0500

    Small Start on IntakeIOSim.java

--- src/main/java/frc/WorBots/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/WorBots/subsystems/intake/IntakeIOSim.java
@@ -1,5 +1,18 @@
 package frc.WorBots.subsystems.intake;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import frc.WorBots.Constants;
+
+//import frc.WorBots.util.RobotSimulator;
+
 public class IntakeIOSim implements IntakeIO{
-    
+    private FlywheelSim intakeSim;
+    private FlywheelSim extendingSim;
+
+
+    public IntakeIOSim(){
+        intakeSim = new FlywheelSim(null, null, null);
+        extendingSim = new FlywheelSim(null, null, null);
+    }
 }
