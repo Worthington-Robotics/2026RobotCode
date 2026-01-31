67505ba4d6ed07e7bb52190bfccf114e5f1a63b0
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-31 13:12:02 -0500

    ToF Related Constants and Variables
    In IntakeIO.java and C

--- src/main/java/frc/WorBots/Constants.java
+++ src/main/java/frc/WorBots/Constants.java
@@ -38,5 +38,6 @@ public class Constants {
   //These values need to be modified
   public static final double INTAKE_MAX_TEMP = 80.0;
   public static final double INTAKE_VOLTS = 4.25;
-  
+  public static final double TIME_OF_FLIGHT_THRES = 0.255;
+
 }
--- src/main/java/frc/WorBots/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/WorBots/subsystems/intake/IntakeIO.java
@@ -5,6 +5,7 @@ import frc.WorBots.util.HardwareUtils.TalonInputsPositional;
 public interface IntakeIO {
     public static class IntakeIOInputs{ 
         TalonInputsPositional motor = new TalonInputsPositional("Intake", "Motor");
+        double timeOfFlightDistMeters = 0.0;
         boolean isConnected = false;
         double currentDraw = 0.0;
 
