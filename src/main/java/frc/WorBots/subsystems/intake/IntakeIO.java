09c49c14a90392da1279a2293793bf94551e945e
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-31 11:17:08 -0500

    IntakeIO

--- src/main/java/frc/WorBots/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/WorBots/subsystems/intake/IntakeIO.java
@@ -1,5 +1,20 @@
 package frc.WorBots.subsystems.intake;
+import frc.WorBots.util.HardwareUtils.TalonInputsPositional;
+
 
 public interface IntakeIO {
-    
+    public static class IntakeIOInputs{ 
+        TalonInputsPositional motor = new TalonInputsPositional("Intake", "Motor");
+        boolean isConnected = false;
+        double currentDraw = 0.0;
+
+
+
+    }
+
+    public default void setIntakeVolts(double volts){}
+
+    public default void updateInputs(IntakeIOInputs inputs){}
+
+
 }
