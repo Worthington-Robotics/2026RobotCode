6fd97940ace54a23b5f4e38e0ae2bafea99f52d8
berilsakin09 <illcecileyourmana@gmail.com>
2026-01-31 13:54:46 -0500

    Created IntakeCommands.java

---
+++ src/main/java/frc/WorBots/commands/IntakeCommands.java
@@ -0,0 +1,11 @@
+package frc.WorBots.commands;
+
+public class IntakeCommands {
+    public void intake(){}
+
+    public void spit(){}
+
+    public void extend(){}
+
+    public void retract(){}
+}
