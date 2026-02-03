0cc977eaf9317a289d090cdb1d37ce30c841b947
berilsakin09 <illcecileyourmana@gmail.com>
2026-02-03 16:45:14 -0500

    Intake Commands

--- src/main/java/frc/WorBots/commands/IntakeCommands.java
+++ src/main/java/frc/WorBots/commands/IntakeCommands.java
@@ -1,9 +1,25 @@
 package frc.WorBots.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.WorBots.subsystems.intake.*;
+
 public class IntakeCommands {
-    public void intake(){}
+    public Command intake(Intake intake) {
+        // Does not end on its own, unless something else cancels it.
+        return intake.startEnd(() -> {
+            intake.setVoltsIntake(5);
+        }, () -> {
+            intake.setVoltsIntake(0);
+        });
 
-    public void extend(){}
+    }
 
-    public void retract(){}
+    public Command extend(Intake extend){
+        return extend.runOnce(() -> {extend.setVoltsExtending(5);});
+    }
+
+    public Command retract(Intake extend) {
+        return extend.runOnce(()-> {extend.setVoltsExtending(-5);});
+    }
 }
