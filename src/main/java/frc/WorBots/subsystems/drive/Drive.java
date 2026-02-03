e54a88d67dd38bb3b0247914031a8e24fbd25e09
Non specific programmer 4 <worbotsprogrammer@gmail.com>
2026-02-03 17:42:33 -0500

    Minor Drive Tweaks

--- src/main/java/frc/WorBots/subsystems/drive/Drive.java
+++ src/main/java/frc/WorBots/subsystems/drive/Drive.java
@@ -40,9 +40,7 @@ public class Drive extends SubsystemBase{
 
   private ChassisSpeeds setpointSpeeds = new ChassisSpeeds();
 
-  private Twist2d fieldVelocity = new Twist2d();
-
-  private ChassisSpeeds measurdSpeeds;
+  private ChassisSpeeds measuredSpeeds;
 
   private Rotation2d lastGyroYaw = new Rotation2d();
 
@@ -176,7 +174,7 @@ public class Drive extends SubsystemBase{
     SwerveModuleState[] setpointStates = new SwerveModuleState[4];
     
     for(int i = 0; i < 4; i++){
-      setpointStates[i] = new SwerveModuleState(0.0, new Rotation2d(StopMode.BlockAll.moduleAngles[i]));
+      setpointStates[i] = new SwerveModuleState(0.0, modules[i].getAngle() );
     }
     return setpointStates;
   }
@@ -221,23 +219,14 @@ public class Drive extends SubsystemBase{
    */
   public void updateOdometry(){
     final double startTime = Timer.getFPGATimestamp();
-    SwerveModuleState[] meauredStates = new SwerveModuleState[4];
+    SwerveModuleState[] measuredStates = new SwerveModuleState[4];
 
     for(int i=0; i<4; i++){
-      meauredStates[i] = modules[i].getState();
+      measuredStates[i] = modules[i].getState();
     }
-    moduleMeasuredPublisher.set(Logger.statesToArray(meauredStates));
+    moduleMeasuredPublisher.set(Logger.statesToArray(measuredStates));
 
-    measurdSpeeds = kinematics.toChassisSpeeds(meauredStates);
-    final Translation2d linearFieldVelocity =
-      new Translation2d(measurdSpeeds.vxMetersPerSecond, measurdSpeeds.vyMetersPerSecond)
-      .rotateBy(getRotation());
-    
-    fieldVelocity = new Twist2d(
-      linearFieldVelocity.getX(),
-      linearFieldVelocity.getY(),
-      gyroIOInputs.connected
-        ? gyroIOInputs.yawVelocityRadPerSec : measurdSpeeds.omegaRadiansPerSecond);
+    measuredSpeeds = kinematics.toChassisSpeeds(measuredStates);
 
     OdometryThread.odometryLock.lock();
     ArrayList<Double> timestamps = new ArrayList<>(OdometryThread.timestampQueue.size());
@@ -247,7 +236,6 @@ public class Drive extends SubsystemBase{
       final double timestamp = OdometryThread.timestampQueue.poll();
       timestamps.add(timestamp);
     }
-    SmartDashboard.putNumberArray("timestamps", timestamps.toArray(new Double[0]));
 
     OdometryThread.odometryLock.unlock();
 
@@ -272,9 +260,7 @@ public class Drive extends SubsystemBase{
         modulus = 3;
       }
     }
-    SmartDashboard.putNumber("minSize", minSize);
     int update = 0;
-    double temp[] = new double[3];
     for(update = 0; update < minSize / modulus; update++){
       update = update * modulus;
 
@@ -295,24 +281,17 @@ public class Drive extends SubsystemBase{
       if(gyroIOInputs.connected) {
         final double dtheta = gyroYaw.minus(lastGyroYaw).getRadians();
 
-        SmartDashboard.putNumber("Odometry dTheta Error", dtheta - twist.dtheta);
         twist.dtheta = dtheta;
       }
 
       lastGyroYaw = gyroYaw;
 
       poseEstimator.addDriveDataNoUpdate(timestamps.get(update), twist);
-      temp[0] = twist.dx;
-      temp[1] = twist.dy;
-      temp[2] = twist.dtheta;
       posePublisher.set(getPose());
 
-      gyroIO.setExpectedYawVelocity(measurdSpeeds.omegaRadiansPerSecond);
+      gyroIO.setExpectedYawVelocity(measuredSpeeds.omegaRadiansPerSecond);
     }
-
-    SmartDashboard.putNumber("odometry update", update);
-    SmartDashboard.putNumberArray("odometry update twist", temp);
-
+    measuredSpeedPublisher.set(measuredSpeeds);
     poseEstimator.update();
 
     final double endTime = Timer.getFPGATimestamp();
