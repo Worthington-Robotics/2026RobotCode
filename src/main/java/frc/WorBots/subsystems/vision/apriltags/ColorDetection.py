8dcba9a1302e78bbf55f78b9a2ab40076872a548
berilsakin09 <illcecileyourmana@gmail.com>
2026-02-17 17:38:05 -0500

    Minor mods, no change in function

--- src/main/java/frc/WorBots/subsystems/vision/apriltags/ColorDetection.py
+++ src/main/java/frc/WorBots/subsystems/vision/apriltags/ColorDetection.py
@@ -24,11 +24,11 @@ while(1):
 	# color space 
 	hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV) 
 
-	# Set range for red color and 
+	# Set range for yellow color and 
 	# define mask 
-	red_lower = np.array([136, 87, 111], np.uint8) 
-	red_upper = np.array([180, 255, 255], np.uint8) 
-	red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
+	fuel_lower = np.array([136, 87, 111], np.uint8) 
+	fuel_upper = np.array([180, 255, 255], np.uint8) 
+	fuel_mask = cv2.inRange(hsvFrame, fuel_lower, fuel_upper) 
 
 	
 	# Morphological Transform, Dilation 
@@ -37,14 +37,14 @@ while(1):
 	# to detect only that particular color 
 	kernel = np.ones((5, 5), "uint8") 
 	
-	# For red color 
-	red_mask = cv2.dilate(red_mask, kernel) 
-	res_red = cv2.bitwise_and(imageFrame, imageFrame, 
-							mask = red_mask) 
+	# For yellow color 
+	fuel_mask = cv2.dilate(fuel_mask, kernel) 
+	res_fuel = cv2.bitwise_and(imageFrame, imageFrame, 
+							mask = fuel_mask) 
 	
 
-	# Creating contour to track red color 
-	contours, hierarchy = cv2.findContours(red_mask, 
+	# Creating contour to track yellow color 
+	contours, hierarchy = cv2.findContours(fuel_mask, 
 										cv2.RETR_TREE, 
 										cv2.CHAIN_APPROX_SIMPLE) 
 	
@@ -56,14 +56,14 @@ while(1):
 									(x + w, y + h), 
 									(0, 0, 255), 2) 
 			
-			cv2.putText(imageFrame, "Red Colour", (x, y), 
+			cv2.putText(imageFrame, "Yellow Colour", (x, y), 
 						cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
 						(0, 0, 255))	 
 
 
 			
 	# Program Termination 
-	cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame) 
+	cv2.imshow("Fuel Detection in Real-TIme", imageFrame) 
 	if cv2.waitKey(10) & 0xFF == ord('q'): 
 		webcam.release() 
 		cv2.destroyAllWindows() 
