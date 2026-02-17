8d80319e6006468982d0b86547bd62d70fbac7a3
ih0027 <ihershhorn@icloud.com>
2026-02-17 17:53:25 -0500

    Removed unused import

--- src/main/java/frc/WorBots/commands/ShooterAuto.java
+++ src/main/java/frc/WorBots/commands/ShooterAuto.java
@@ -1,7 +1,5 @@
 package frc.WorBots.commands;
 
-import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate.Param;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.WorBots.FieldConstants;
