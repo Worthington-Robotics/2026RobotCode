2f04c8051c5718b788a52c0b2680998f3b9184c9
ih0027 <ihershhorn@icloud.com>
2026-02-25 16:30:56 -0500

    Removed unused imports

--- src/main/java/frc/WorBots/commands/ShooterTest.java
+++ src/main/java/frc/WorBots/commands/ShooterTest.java
@@ -2,8 +2,6 @@ package frc.WorBots.commands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
--- src/main/java/frc/WorBots/subsystems/turret/Turret.java
+++ src/main/java/frc/WorBots/subsystems/turret/Turret.java
@@ -14,9 +14,7 @@ import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StringPublisher;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.WorBots.Constants;
 import frc.WorBots.subsystems.shooter.ShotCalculator.ShootingParams;
 import frc.WorBots.subsystems.turret.TurretIO.TurretIOInputs;
 
