f7a794d2bbb8e43575253c7ff301e9d23fabe635
Non-specific programmer 6 <worbotsprogrammer@gmail.com>
2026-02-28 12:01:23 -0500

    Update FieldConstants.java

--- src/main/java/frc/WorBots/FieldConstants.java
+++ src/main/java/frc/WorBots/FieldConstants.java
@@ -5,7 +5,4 @@ import edu.wpi.first.math.util.Units;
 public class FieldConstants {
     public static final double fieldWidth = Units.inchesToMeters(317.69);
     public static final double fieldLength = Units.inchesToMeters(651.22);
-
-    public static final double BLUE_ZONE_LINE_X_CORD = Units.inchesToMeters(20);
-    public static final double RED_ZONE_LINE_X_CORD = Units.inchesToMeters(40);
 }
