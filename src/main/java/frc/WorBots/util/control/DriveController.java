4530c519f7fcd010d98c6a4ea41a90296c31908d
Non-specific programmer 5 <worbotsprogrammer@gmail.com>
2026-04-23 17:32:23 -0400

    Minor Drive adjustment for Alex

--- src/main/java/frc/WorBots/util/control/DriveController.java
+++ src/main/java/frc/WorBots/util/control/DriveController.java
@@ -33,7 +33,7 @@ public class DriveController {
   public static final double DRIVE_SPEED_MULTIPLIER = 0.61;
 
   /** The max rotational speed in radians per update that the robot will drive at */
-  public static final double ROTATIONAL_SPEED = 3.55 * 0.95; //Used to be 3.75, turned down on driver request
+  public static final double ROTATIONAL_SPEED = 3.55 * 1.0; //Used to be 3.75, turned down on driver request
 
   /** The amount of input deadband to apply */
   public static final double DEADBAND = 0.15;
